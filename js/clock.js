febbb0be426a7441d6f6fd73b6b1995b0246193c
Hyeok Jun Kim <hyeokjun97@gmail.com>
2021-08-03 12:41:37 +0900

    Momentum App_5.2

--- js/clock.js
+++ js/clock.js
@@ -1,8 +1,8 @@
-const clocks = document.querySelector("#h2clocks");
+const clock = document.querySelector("h2#clock");
 
 function getClock(){
     const date = new Date();
-    clocks.innerText =`${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+    clock.innerText =`${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
 }
 
 getClock();
