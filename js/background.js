f998322b73cfcd80fceaa17a475d9b2d29e4f275
Hyeok Jun Kim <hyeokjun97@gmail.com>
2021-08-03 14:09:05 +0900

    Background Pictures added with images

---
+++ js/background.js
@@ -0,0 +1,11 @@
+const images = [
+    "0.jpg", "1.jpg", "2.jpg", "3.jpg", "4.jpg"
+];
+
+const chosenImage = images[Math.floor(Math.random()*images.length)];
+
+const bgImage = document.createElement("img");
+bgImage.src = `img/${chosenImage}`;
+//images from google.
+
+document.body.appendChild(bgImage);
