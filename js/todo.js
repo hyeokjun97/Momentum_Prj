00d1f3cb02547e344ea2a3680455b64a606cac5e
Hyeok Jun Kim <hyeokjun97@gmail.com>
2021-08-03 20:45:58 +0900

    Todo list backup from local storage

--- js/todo.js
+++ js/todo.js
@@ -1,6 +1,14 @@
 const toDoForm = document.getElementById("todo-form");
 const toDoInput = document.querySelector("#todo-form input");
 const toDoList = document.getElementById("todo-list");
+const TODOS_KEY = "todos";
+let toDos = [];
+
+function saveToDos(){
+
+    localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
+}
+
 function deleteToDo(event){
     const li = event.target.parentElement;
     li.remove();
@@ -23,9 +31,17 @@ function handleToDoSubmit(event){
     event.preventDefault();
     const newTodo = toDoInput.value;
     toDoInput.value = "";
+    toDos.push(newTodo);
     paintToDo(newTodo);
-
+    saveToDos();
 }
 
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
+const savedToDos = localStorage.getItem(TODOS_KEY);
+
+if(saveToDos!==null){
+    const parsedToDos = JSON.parse(savedToDos);
+    toDos = parsedToDos;
+    parsedToDos.forEach(paintToDo); 
+}
